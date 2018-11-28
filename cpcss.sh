650f8d0057905cf0900d704f59a92c7f9d9f9a2c
mmaranda-cs <mmaranda@camsys.com>
2018-11-28 15:27:30 -0500

    Second commit with a mess of things to try and get the app working when deployed to an apache server

--- cpcss.sh
+++ cpcss.sh
@@ -1,5 +1,5 @@
 echo "building and copying css.."
 mastarm build
-cp ~/dev/otp/otp-react-redux/dist/index.css* ~/dev/otp/trimet-mod-otp/node_modules/otp-react-redux/dist/
+cp /dist/index.css* /node_modules/otp-react-redux/dist/
 echo "done"
 

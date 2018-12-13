84d603501d3ced5f728d894ef2e9eb80bd957e34
Tazeen Mahtab <tmahtab@camsys.com>
2018-12-13 10:16:37 -0800

    Updates to build scripts.

--- cpbuild.sh
+++ cpbuild.sh
@@ -1,5 +1,5 @@
 echo 'rebuilding..'
 yarn prepublish
 echo 'copying built js..'
-cp -r ./build ~/dev/otp/trimet-mod-otp/node_modules/otp-react-redux
+cp -r ./build /node_modules/otp-react-redux/dist/
 echo 'done'
